974af28eeb790e290ebba7a15ac4c41dd763056e
Rosie Sapphire <jayjaygames40@gmail.com>
2023-05-06 15:20:54 -0500

    Added camera movement with the mouse instead of keyboard

--- src/camera.c
+++ src/camera.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
 #include <math.h>
 #include "camera.h"
+#include "rmath/helpers.h"
 #include "rmath/mat4.h"
 #include "rmath/vec3f.h"
 
 void camera_get_look_pos(struct camera c, rm_vec3f out)
 {
-	rm_vec3f look_pos = {cosf(c.yaw), 0.0f, sinf(c.yaw)};
+	rm_vec3f look_pos = {
+		cosf(c.yaw * RM_PI) * cosf(c.pitch * RM_PI),
+		sinf(c.pitch * RM_PI),
+		sinf(c.yaw * RM_PI) * cosf(c.pitch * RM_PI),
+	};
 
 	rm_vec3f_add(c.eye_pos, look_pos, out);
 }
@@ -37,22 +44,30 @@ void camera_get_view_mat4(struct camera c, rm_mat4 out)
 	camera_get_forward_vec(c, look_pos);
 }
 
+void camera_update_rotation(struct camera *c, GLFWwindow *win, float sens)
+{
+	double mouse_x, mouse_y;
+
+	glfwGetCursorPos(win, &mouse_x, &mouse_y);
+	mouse_y = rm_clampf(mouse_y, -830, 830);
+	glfwSetCursorPos(win, mouse_x, mouse_y);
+
+	c->yaw = mouse_x * sens;
+	c->pitch = mouse_y * sens;
+}
+
 void camera_update_position(struct camera *c, GLFWwindow *win, float dt)
 {
 	float move_speed = RM_PI * dt;
-	float turn_speed = RM_PI * 0.5f * dt;
 
 	if(glfwGetKey(win, GLFW_KEY_LEFT_SHIFT)) {
 		move_speed *= 2;
-		turn_speed *= 2;
 	}
 
-	float turn_amount = (glfwGetKey(win, GLFW_KEY_D) -
-			glfwGetKey(win, GLFW_KEY_A)) * turn_speed;
 	float forward_move = (glfwGetKey(win, GLFW_KEY_S) -
 			glfwGetKey(win, GLFW_KEY_W));
-	float right_move = (glfwGetKey(win, GLFW_KEY_Q) -
-			glfwGetKey(win, GLFW_KEY_E));
+	float right_move = (glfwGetKey(win, GLFW_KEY_A) -
+			glfwGetKey(win, GLFW_KEY_D));
 
 	rm_vec3f forward_vec;
 	rm_vec3f right_vec;
@@ -67,6 +82,4 @@ void camera_update_position(struct camera *c, GLFWwindow *win, float dt)
 	rm_vec3f_normalize(move_vec);
 	rm_vec3f_scale(move_vec, move_speed);
 	rm_vec3f_add(c->eye_pos, move_vec, c->eye_pos);
-
-	c->yaw += turn_amount;
 }
